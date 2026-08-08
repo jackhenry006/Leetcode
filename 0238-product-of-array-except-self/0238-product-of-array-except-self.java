dc26b5d840065a38467bca8f26ad03d251503112
Ankit Kumar Nayak <ankitkumarnayak006@gmail.com>
2026-08-09 02:34:00 +0530

    Time: 2 ms (94.02%), Space: 72.1 MB (41.88%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,14 +3,14 @@ class Solution {
         int n = nums.length;
         int[] result = new int[n];
         
-        // Pass 1: left products
+        // P
         int left = 1;
         for (int i = 0; i < n; i++) {
             result[i] = left;
             left = left * nums[i];
         }
         
-        // Pass 2: multiply in right products
+       
         int right = 1;
         for (int i = n - 1; i >= 0; i--) {
             result[i] = result[i] * right;
