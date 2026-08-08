e32b67bcfd207b2743ce85b1a42ca86e6450a505
Ankit Kumar Nayak <ankitkumarnayak006@gmail.com>
2026-08-08 23:30:33 +0530

    Time: 2 ms (42.02%), Space: 94.5 MB (51.96%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+    
+    int min=prices[0];
+    int max = 0;
+
+    for(int i=1;i<prices.length;i++){
+        if(prices[i]<min){
+            min=prices[i];
+        }
+            int today = prices[i]-min;
+            if(today>max){
+                max=today;
+            }
+            
+        
+
+    }
+return max;
+    }
+}
